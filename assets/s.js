4caff55cc581bc6ef51ae9f2ec3f517eccaae65f
RAMU <athmakooriramu@gmail.com>
2020-04-11 20:20:00 +0530

    commited

---
+++ assets/s.js
@@ -0,0 +1,202 @@
+function openPage(pageName,elmnt,color) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].style.backgroundColor = "";
+    
+    document.getElementById(pageName).style.display = "block";
+    elmnt.style.backgroundColor = color;
+	}
+
+}
+function sum() {
+ var txt1 = document.getElementById('1').value;
+        var result1 = parseInt(txt1) * parseInt(6);
+            if (!isNaN(result1)) {
+                document.getElementById('2').value = result1;}
+var txt2 = document.getElementById('3').value;
+        var result2 = parseInt(txt2) * parseInt(30);
+            if (!isNaN(result2)) {
+                document.getElementById('4').value = result2;
+            }
+ var txt3 = document.getElementById('5').value;
+        var result3 = parseInt(txt3) * parseInt(600);
+            if (!isNaN(result3)) {
+                document.getElementById('6').value = result3;}
+var txt4 = document.getElementById('7').value;
+        var result4 = parseInt(txt4) * parseInt(400);
+            if (!isNaN(result4)) {
+                document.getElementById('8').value = result4;
+            }
+ var txt5 = document.getElementById('9').value;
+        var result5 = parseInt(txt5) * parseInt(300);
+            if (!isNaN(result5)) {
+                document.getElementById('10').value = result5;}
+var txt6 = document.getElementById('11').value;
+        var result6 = parseInt(txt6) * parseInt(200);
+            if (!isNaN(result6)) {
+                document.getElementById('12').value = result6;
+            }
+ var txt7 = document.getElementById('13').value;
+        var result7 = parseInt(txt7) * parseInt(50);
+            if (!isNaN(result7)) {
+                document.getElementById('14').value = result7;}
+var txt8 = document.getElementById('15').value;
+        var result8 = parseInt(txt8) * parseInt(100);
+            if (!isNaN(result8)) {
+                document.getElementById('16').value = result8;
+            }
+ var txt9 = document.getElementById('17').value;
+        var result9 = parseInt(txt9) * parseInt(360);
+            if (!isNaN(result9)) {
+                document.getElementById('18').value = result9;}
+var txt10 = document.getElementById('19').value;
+        var result10 = parseInt(txt10) * parseInt(300);
+            if (!isNaN(result10)) {
+                document.getElementById('20').value = result10;
+            }
+ var txt11 = document.getElementById('21').value;
+        var result11 = parseInt(txt11) * parseInt(240);
+            if (!isNaN(result11)) {
+                document.getElementById('22').value = result11;}
+var txt12 = document.getElementById('23').value;
+        var result12 = parseInt(txt12) * parseInt(180);
+            if (!isNaN(result12)) {
+                document.getElementById('24').value = result12;
+            }
+ var txt13 = document.getElementById('25').value;
+        var result13 = parseInt(txt13) * parseInt(150);
+            if (!isNaN(result13)) {
+                document.getElementById('26').value = result13;}
+var txt14 = document.getElementById('27').value;
+        var result14 = parseInt(txt14) * parseInt(120);
+            if (!isNaN(result14)) {
+                document.getElementById('28').value = result14;
+            }
+ var txt15 = document.getElementById('29').value;
+        var result15 = parseInt(txt15) * parseInt(90);
+            if (!isNaN(result15)) {
+                document.getElementById('30').value = result15;}
+var txt16 = document.getElementById('31').value;
+        var result16 = parseInt(txt16) * parseInt(60);
+            if (!isNaN(result16)) {
+                document.getElementById('32').value = result16;
+            }
+ var txt17 = document.getElementById('33').value;
+        var result17 = parseInt(txt17) * parseInt(30);
+            if (!isNaN(result17)) {
+                document.getElementById('34').value = result17;}
+var txt18 = document.getElementById('35').value;
+        var result18 = parseInt(txt18) * parseInt(500);
+            if (!isNaN(result18)) {
+                document.getElementById('36').value = result18;
+            }
+ var txt19= document.getElementById('37').value;
+        var result19 = parseInt(txt19) * parseInt(400);
+            if (!isNaN(result19)) {
+                document.getElementById('38').value = result19;}
+var txt20 = document.getElementById('39').value;
+        var result20 = parseInt(txt20) * parseInt(20);
+            if (!isNaN(result20)) {
+                document.getElementById('40').value = result20;
+            }
+ var txt21 = document.getElementById('41').value;
+        var result21 = parseInt(txt21) * parseInt(10);
+            if (!isNaN(result21)) {
+                document.getElementById('42').value = result21;}
+var txt22 = document.getElementById('43').value;
+        var result22 = parseInt(txt22) * parseInt(10);
+            if (!isNaN(result22)) {
+                document.getElementById('44').value = result22;
+            }
+ var txt23 = document.getElementById('45').value;
+        var result23 = parseInt(txt23) * parseInt(5);
+            if (!isNaN(result23)) {
+                document.getElementById('46').value = result23;}
+var txt24 = document.getElementById('47').value;
+        var result24 = parseInt(txt24) * parseInt(5);
+            if (!isNaN(result24)) {
+                document.getElementById('48').value = result24;
+            }
+ var txt25 = document.getElementById('49').value;
+        var result25 = parseInt(txt25) * parseInt(5);
+            if (!isNaN(result25)) {
+                document.getElementById('50').value = result25;}
+var txt26 = document.getElementById('51').value;
+        var result26 = parseInt(txt26) * parseInt(20);
+            if (!isNaN(result26)) {
+                document.getElementById('52').value = result26;
+            }
+ var txt27 = document.getElementById('53').value;
+        var result27 = parseInt(txt27) * parseInt(30);
+            if (!isNaN(result27)) {
+                document.getElementById('54').value = result27;}
+var txt28 = document.getElementById('55').value;
+        var result28 = parseInt(txt28) * parseInt(30);
+            if (!isNaN(result28)) {
+                document.getElementById('56').value = result28;
+            }
+ var txt29 = document.getElementById('57').value;
+        var result29 = parseInt(txt29) * parseInt(50);
+            if (!isNaN(result29)) {
+                document.getElementById('58').value = result29;}
+var txt30 = document.getElementById('59').value;
+        var result30 = parseInt(txt30) * parseInt(100);
+            if (!isNaN(result30)) {
+                document.getElementById('60').value = result30;
+            }
+ var txt31 = document.getElementById('61').value;
+        var result31 = parseInt(txt31) * parseInt(800);
+            if (!isNaN(result31)) {
+                document.getElementById('62').value = result31;}
+var txt32 = document.getElementById('63').value;
+        var result32 = parseInt(txt32) * parseInt(100);
+            if (!isNaN(result32)) {
+                document.getElementById('64').value = result32;
+            }
+ var txt33 = document.getElementById('65').value;
+        var result33 = parseInt(txt33) * parseInt(10);
+            if (!isNaN(result33)) {
+                document.getElementById('66').value = result33;}
+var txt34 = document.getElementById('67').value;
+        var result34 = parseInt(txt34) * parseInt(100);
+            if (!isNaN(result34)) {
+                document.getElementById('68').value = result34;
+            }
+ var txt35 = document.getElementById('69').value;
+        var result35 = parseInt(txt35) * parseInt(50);
+            if (!isNaN(result35)) {
+                document.getElementById('70').value = result35;}
+var txt36 = document.getElementById('71').value;
+        var result36 = parseInt(txt36) * parseInt(100);
+            if (!isNaN(result36)) {
+                document.getElementById('72').value = result36;
+            }
+ 
+														
+var txt37 = document.getElementById('73').value;
+        var result37 = parseInt(txt37) * parseInt(100);
+            if (!isNaN(result37)) {
+                document.getElementById('74').value = result37;
+            }
+var txt38 = document.getElementById('75').value;
+        var result38 = parseInt(txt38) * parseInt(100);
+            if (!isNaN(result38)) {
+                document.getElementById('76').value = result38;
+            }														
+     var res = parseInt(result1) + parseInt(result2)+parseInt(result3) + parseInt(result4)+parseInt(result5) + parseInt(result6)+parseInt(result7) + parseInt(result8)+parseInt(result9) + parseInt(result10)+parseInt(result11) + parseInt(result12)+parseInt(result13) + parseInt(result14)+parseInt(result15) + parseInt(result16)+parseInt(result17) + parseInt(result18)+parseInt(result19) + parseInt(result20)+parseInt(result21) + parseInt(result22)+parseInt(result23) + parseInt(result24)+parseInt(result25) + parseInt(result26)+parseInt(result27) + parseInt(result28)+parseInt(result29) + parseInt(result30)+parseInt(result31) + parseInt(result32)+parseInt(result33) + parseInt(result34)+parseInt(result35) + parseInt(result36) + parseInt(result37) + parseInt(result38);
+	    
+if (!isNaN(res)) {
+                document.getElementById('total').value = res;
+            }  
+ }
+ function my() {
+ window.print();}
+// Get the element with id="defaultOpen" and click on it
+
+document.getElementById("defaultOpen").click();
+
